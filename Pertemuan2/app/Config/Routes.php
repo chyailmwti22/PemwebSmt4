9473bfa0b28328433b876f18930cb319c0a1776b
chyailmwti22 <jombang.jatim30@gmail.com>
2024-05-13 14:54:02 +0700

    validation and delete data in ci

--- Pertemuan2/app/Config/Routes.php
+++ Pertemuan2/app/Config/Routes.php
@@ -6,7 +6,17 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Page::index');
-//$routes->get('/', 'Page::about);
-//$routes->setAutoRoute(true);
 
-//$routes->get('/Buku/(:segment)', 'Buku::Detail/$1');
+$routes->get('/books/index', 'Books::index');
+
+$routes->get('/books/create', 'Books::create');
+
+$routes->get('/books/edit/(:segment)', 'Books::edit/$1');
+
+$routes->delete('/books/(:num)', 'Books::delete/$1');
+
+$routes->post('save', 'Books::save');
+
+//$routes->get('/', 'Page::contact');
+
+$routes->get('/books/(:any)', 'Books::detail/$1');
